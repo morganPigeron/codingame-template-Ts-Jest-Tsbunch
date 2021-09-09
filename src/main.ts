e62c353d43885930a35973531a3c4374f7b54ab3
Morgan Pigeron <m.pigeron@hotmail.fr>
2021-09-09 20:11:12 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs: string[] = readline().split(' ');
+const W: number = parseInt(inputs[0]);
+const H: number = parseInt(inputs[1]);
+const N: number = parseInt(inputs[2]);
+const O: string = readline();
+for (let i = 0; i < H; i++) {
+    const line: string = readline();
+}
+for (let i = 0; i < N; i++) {
+    var inputs: string[] = readline().split(' ');
+    const k: string = inputs[0];
+    const x: number = parseInt(inputs[1]);
+    const y: number = parseInt(inputs[2]);
+}
